9526de29eaf6d89db7e841c7e674711b140057a3
Thiago-ferreira17 <thiago.pericles@hotmail.com>
2024-06-03 17:51:53 -0300

    finalizando o back-end

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -7,23 +7,33 @@ class UserController {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
       email: Yup.string().required().email(),
-      password_hash: Yup.string().required().min(6),
+      password: Yup.string().required().min(6),
       admin: Yup.boolean(),
     });
 
     try {
       await schema.validateSync(request.body, { abortEarly: false });
     } catch (err) {
-      return response.status(400).json({ error: err.erros });
+      return response.status(400).json({ error: err.errors });
     }
 
-    const { name, email, password_hash, admin } = request.body;
+    const { name, email, password, admin } = request.body;
+
+    const useExists = await User.findOne({
+      where: { email },
+    });
+
+    if (useExists) {
+      return response.status(400).json({ error: 'User already exists' });
+    }
+
+    console.log(useExists);
 
     const user = await User.create({
       id: v4(),
       name,
       email,
-      password_hash,
+      password,
       admin,
     });
 
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer'
 
 import UserController from './app/controllers/UserController';
+import SessionsController from './app/controllers/SessionsController';
+import ProductController from './app/controllers/ProductController';
 
+const upload = multer(multerConfig)
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+
+routes.post('/sessions', SessionsController.store);
+
+routes.post('/products', upload.single('file'),ProductController.store);
 
 export default routes;
