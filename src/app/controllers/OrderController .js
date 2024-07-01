4d2c9e9d2d07fea2cb1e6fc110ed6db116b9db9a
Thiago-ferreira17 <thiago.pericles@hotmail.com>
2024-07-01 15:57:57 -0300

    modified:   src/app/controllers/OrderController .js

--- src/app/controllers/OrderController .js
+++ src/app/controllers/OrderController .js
@@ -1,34 +1,36 @@
 import * as Yup from 'yup';
 import Order from '../schemas/Order';
 
-
-class OrderController{
+class OrderController {
   async store(request, response) {
-    //const schema = Yup.object().shape({
-     // name: Yup.string().required(),    
-   // });
+    const schema = Yup.object().shape({
+      products: Yup.array()
+        .required()
+        .of(
+          Yup.object().shape({
+            id: Yup.number.required(),
+            quantity: Yup.number.required(),
+          }),
+        ),
+    });
 
-   // try {
-  //    await schema.validateSync(request.body, { abortEarly: false });
-  //  } catch (err) {
-    //  return response.status(400).json({ error: err.errors });
-    //}
-
-   
-   // const { name } = request.body;
-   
-    const order = {
-      user:{
-        id: request.userId ,
-        name: request.userName,
+    try {
+      await schema.validateSync(request.body, { abortEarly: false });
+    } catch (err) {
+      return response.status(400).json({ error: err.errors });
     }
 
-    return response.json(order);
+    const { name } = request.body;
+
+    const order = {
+      user: {
+        id: request.userId,
+        name: request.userName,
+      },
+    };
+
+    return response.json(request.body);
   }
-
-  
 }
-
-
 
 export default new OrderController();
